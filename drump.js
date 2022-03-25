27123e9926e4bb4ae7637f64f5b7b79a185853e3
Vishal kumar <87267576+Vishal7547@users.noreply.github.com>
2022-03-25 13:43:29 +0530

    musicCommit

---
+++ drump.js
@@ -0,0 +1,16 @@
+var p = document.getElementById('one1');
+function run(){
+    p.play();
+}
+var q = document.getElementById('one2');
+function run1(){
+    q.play();
+}
+var r = document.getElementById('one3');
+function run2(){
+    r.play();
+}
+var s = document.getElementById('one4');
+function run3(){
+    s.play();
+}
